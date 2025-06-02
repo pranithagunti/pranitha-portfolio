33082e09f025848adaf07666923c23b31121217e
pranithagunti16 <pranitha.gunti1609@gmail.com>
2025-06-02 17:48:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Set base to /mywebsite/ (your repo name)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/mywebsite/',
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  base:"/pranitha-portfolio",
+});
